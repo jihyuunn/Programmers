246ce00b3c580a702f2928f7b96652a02a5db5c2
jihyuunn <791004_@likelion.org>
2020-06-12 11:23:17 +0900

    🙋🏻‍♀️ 문제풀이

---
+++ javascript/_19.js
@@ -0,0 +1,17 @@
+function solution(people, limit) {
+    var answer = 0;
+    people = people.sort((a,b) => a-b);
+    let left = 0;
+    let right = people.length -1;
+    while (left<=right) {
+        if (people[left]+people[right]<=limit) {
+            answer += 1
+            left += 1
+            right -= 1
+        } else {
+            right -= 1
+            answer += 1
+        }
+    }
+    return answer;
+}
